07093118aa52bd670193a313187fda2488e155f0
Richard Cancino Vilchez <richard.cancinov@gmail.com>
2019-06-23 10:52:51 -0500

    [FIX-2] Fix conf on postgres

--- tres_raya_app/settings/staging.py
+++ tres_raya_app/settings/staging.py
@@ -4,7 +4,7 @@ ALLOWED_HOSTS = ['178.128.9.218', '127.0.0.1', 'localhost']
 DEBUG = False
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'chatydb',
         'USER': 'postgres',
         'PASSWORD': '992424558',
